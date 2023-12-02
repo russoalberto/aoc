0e9c73249bb9cc3aa706b5a0aa23740e326af7a8
Alberto Russo <alberto@albertorusso.it>
2023-12-02 17:29:02 +0100

    feat(2023): add day01

---
+++ 2023/day01_1.lua
@@ -0,0 +1,27 @@
+local input = './input/day01'
+
+local function findFirst(line)
+  for i = 1, #line do
+    if string.find(string.sub(line, i, i), "%d+") then
+      return line:sub(i, i)
+    end
+  end
+end
+
+local function findLast(line)
+  for i = #line, 1, -1 do
+    if string.find(string.sub(line, i, i), "%d+") then
+      return line:sub(i, i)
+    end
+  end
+end
+
+local function main()
+  local total = 0
+  for line in io.lines(input) do
+    total = total + tonumber(findFirst(line) .. findLast(line))
+  end
+  return total
+end
+
+print(main())
---
+++ 2023/day01_2.lua
@@ -0,0 +1,55 @@
+local math = require 'math'
+local io = require 'io'
+local input = './input/day01'
+local validDigits = {
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "%d+"
+}
+
+local function findFirst(line)
+  local index = math.huge
+  local value = ""
+  for i = 1, #validDigits do
+    local current, _ = string.find(line, validDigits[i])
+    if (current and index > current) then
+      index = current
+      value = i > 9 and string.sub(line, current, current) or tostring(i)
+    end
+  end
+
+  return value
+end
+
+local function findLast(line)
+  local reverse = line:reverse()
+  local index = math.huge
+  local value = ""
+  for i = 1, #validDigits do
+    local pattern = i > 9 and validDigits[i] or validDigits[i]:reverse()
+    local current, _ = string.find(reverse, pattern)
+    if (current and index > current) then
+      index = current
+      value = i > 9 and string.sub(reverse, current, current) or tostring(i)
+    end
+  end
+
+  return value
+end
+
+local function main()
+  local total = 0
+  for line in io.lines(input) do
+    total = total + tonumber(findFirst(line) .. findLast(line))
+  end
+  return total
+end
+
+print(main())
